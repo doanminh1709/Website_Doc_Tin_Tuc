d2b89074e9492bb95176494d59a35b2a9480bed6
doanminh1709 <doanducminh11082002@gmail.com>
2023-05-17 09:14:09 +0700

    Website_Doc_Sach

---
+++ src/main/java/doctintuc/com/websitedoctintuc/WebsiteDocTinTucApplication.java
@@ -0,0 +1,13 @@
+package doctintuc.com.websitedoctintuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteDocTinTucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteDocTinTucApplication.class, args);
+    }
+
+}
